0b863d8c3c3aa2a706a25f486e41a3935006cd8a
Patrick Navarro <patricknv230101@gmail.com>
2023-05-21 01:18:11 -0500

    fix: video 2 - create entity and my first migration EF Core

---
+++ FullApiDemo.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using FullApiDemo.Application.Common.Interfaces;
+using FullApiDemo.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullApiDemo.Infrastructure
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(configuration.GetConnectionString("defaultConection"),
+					builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+			);
+
+			services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+			return services;
+		}
+	}
+}
---
+++ FullApiDemo.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FullApiDemo.Application.Common.Interfaces;
+using FullApiDemo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullApiDemo.Infrastructure.Persistence
+{
+	public class ApplicationDbContext : DbContext, IApplicationDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<User> User { get; set; }
+
+		public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			return await base.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
